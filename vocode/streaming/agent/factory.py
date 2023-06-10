c68fb84e91d77273798708f8deb8f6fe4738c624
DanteNoguez <dantenoguez@gmail.com>
2023-06-09 19:03:27 -0600

    factory

--- vocode/streaming/agent/factory.py
+++ vocode/streaming/agent/factory.py
@@ -6,6 +6,7 @@ from vocode.streaming.agent.anthropic_agent import ChatAnthropicAgent
 from vocode.streaming.agent.base_agent import BaseAgent
 from vocode.streaming.agent.chat_gpt_agent import ChatGPTAgent
 from vocode.streaming.agent.echo_agent import EchoAgent
+from vocode.streaming.agent.folks_agent import FolksActionAgent
 from vocode.streaming.agent.information_retrieval_agent import InformationRetrievalAgent
 from vocode.streaming.agent.llm_agent import LLMAgent
 from vocode.streaming.agent.restful_user_implemented_agent import (
@@ -18,6 +19,7 @@ from vocode.streaming.models.agent import (
     ChatAnthropicAgentConfig,
     ChatGPTAgentConfig,
     EchoAgentConfig,
+    FolksActionAgentConfig,
     InformationRetrievalAgentConfig,
     LLMAgentConfig,
     RESTfulUserImplementedAgentConfig,
@@ -42,4 +44,6 @@ class AgentFactory:
             return ChatAnthropicAgent(agent_config=agent_config, logger=logger)
         elif isinstance(agent_config, ActionAgentConfig):
             return ActionAgent(agent_config=agent_config, logger=logger)
+        elif isinstance(agent_config, FolksActionAgentConfig):
+            return FolksActionAgent(agent_config=agent_config, logger=logger)
         raise Exception("Invalid agent config", agent_config.type)
