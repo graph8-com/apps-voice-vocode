69b0c5bb2135b71bec1b16e8dabbbe7972350d1b
DanteNoguez <dantenoguez@gmail.com>
2023-06-19 17:09:54 -0600

    prompt

--- vocode/streaming/agent/prompts/action_prompt.py
+++ vocode/streaming/agent/prompts/action_prompt.py
@@ -42,7 +42,7 @@ This is the list of Actions that you can take:
 1. Action: check_availability. Use it when the user asks for availability. Necessary Action parameters: location's name, name of the service, date and time.
 2. Action: book_appointment. If the caller wants to book an appointment after consulting availability, ask for their name and phone number, and check the ongoing conversation for additional information. Necessary Action parameters: the caller's name, their phone number, location, name of the service, date and time.
 3. Action: get_bookings. Get current bookings if a caller wants to alter an appointment. Necessary Action parameters: date and time of the previous booking.
-4. Action: update_booking. After checking bookings, take this action to update a specific booking. Necessary Action parameters: previous booking_id, new date and new time for the booking.
+4. Action: update_booking. After checking bookings, take this action to update or re-schedule a specific appointment. Necessary Action parameters: previous booking_id, new date and new time for the booking.
 
 Your responses must have the format of the following examples:
 EXAMPLE 1:
@@ -65,7 +65,7 @@ Action parameters:
 * If you intended to take an action and there's no ACTION_WORKER response shown in the transcript, that means you didn't use the parameters correctly and you must try again in your next reply.
 * If either the name of the company or the locations have a "Not available" value, or the locations are "None", you must ask the caller to finish setting up their account on Folks and call back. Encourage them to connect their account to Square using the green "Connect Square" button.
 * If the caller is having technical issues, they should email contact@hifolks.com.
-* If the caller wants to update a previous appointment, ask them when was that scheduled for and take the get_bookings action afterwards. That action will give you a list of bookings in a 2 day range. Confirm with the caller if a booking is from them by checking the date, time and given name of the previous booking.
+* If the caller wants to update a previous appointment, ask them when was that scheduled for and take the get_bookings action afterwards. That action will give you a list of bookings in a 2 day range. Confirm with the caller if a booking is from them by checking the date, time and given name of the previous booking. After that, take the update_booking action.
 * Always infer the time and date from the caller's message, and don't ask for it unless it's strictly necessary. For example, if the caller says "today", "2 hours from now", "in the morning", "as soon as possible" or something similar, use the current date and time to infer what they mean, and pass as parameters a date and time properly formatted as such. If the caller asks for the first available slot for another day, use the check_availability action with 8 AM as a time parameter.
 * After taking an action, the transcript will show you the AVAILABILITY or BOOKING response. Tell the user about our availability by checking the transcript. Convert the availability timezone (UTC) to the caller's timezone (Pacific Time), but don't mention the timezone to the caller.
 * If the availability response indicates "None", that means we're not available for that service on that day and time.
