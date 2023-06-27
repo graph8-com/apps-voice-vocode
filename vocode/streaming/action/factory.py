2552e713ac5c96bc9cae9aa2a2017e87b66697d7
DanteNoguez <dante@dezvolta.com.mx>
2023-06-27 15:31:31 -0600

    scheduler

--- vocode/streaming/action/factory.py
+++ vocode/streaming/action/factory.py
@@ -1,7 +1,7 @@
 from vocode.streaming.action.base_action import BaseAction
 from vocode.streaming.action.nylas_send_email import NylasSendEmail
 from vocode.streaming.models.actions import ActionType
-from vocode.streaming.action.square_actions import GetServices, GetAvailability
+from vocode.streaming.action.square_actions import GetServices, GetAvailability, Scheduler
 
 
 class ActionFactory:
@@ -10,10 +10,10 @@ class ActionFactory:
             return NylasSendEmail(should_respond=True)
         elif action_type == "get_availability":
             return GetAvailability()
-        # elif action_type == "book_appointment":
-        #     return Scheduler()
+        elif action_type == "book_appointment":
+            return Scheduler()
         elif action_type == "get_services":
-            return GetServices(should_respond=True)
+            return GetServices()
         # elif action_type == "get_bookings":
         #     return GetBookings()
         # elif action_type == "update_booking":
--- vocode/streaming/action/square_actions.py
+++ vocode/streaming/action/square_actions.py
@@ -96,12 +96,6 @@ class GetServices(BaseAction[ServicesParameters, ServicesOutput]):
                 return item_names
             else:
                 return "Request failed with status code: ", response.status_code, response.json()
-            
-    def _user_message_param_info(self):
-        return {
-            "type": "string",
-            "description": """Just a moment please.""",
-        }
 
     async def run(
         self, action_input: ActionInput[ServicesParameters]
@@ -204,12 +198,6 @@ class GetAvailability(BaseAction[AvailabilityParameters, AvailabilityOutput]):
                 print("no date or time found")
                 pass
         return date_time_pt_str, date_time_ahead_pt_str
-    
-    def _user_message_param_info(self):
-        return {
-            "type": "string",
-            "description": """Alright. Give me a sec to consult our availability.""",
-        }
 
     async def run(
         self, action_input: ActionInput[AvailabilityParameters]
@@ -225,128 +213,152 @@ class GetAvailability(BaseAction[AvailabilityParameters, AvailabilityOutput]):
             response=AvailabilityOutput(response=str(availability)),
         )
     
+class SchedulerParameters(BaseModel):
+    name: str = Field(..., description="Name of the person that books the appointment")
+    phone: str = Field(..., description="Phone number of the person that books the appointment")
+    location_id: str = Field(..., description="ID corresponding to the single location if there's only one location; otherwise, ID corresponding to the location name selected by the caller.")
+    service: str = Field(..., description="Name of the service that matches the caller's request.")
+    date: str = Field(..., description="Month and day of the month for the appointment, formatted as: June, 5th")
+    time: str = Field(..., description="Desired time for the appointment, formatted as: 8 AM")
+    token: Optional[str] = Field(None, description="token for the API call.")
 
-# class Scheduler(BaseAction[SchedulerOutput]):
-#     def create_customer(self, name, phone, token):
-#         try:
-#             url = 'https://connect.squareup.com/v2/customers'
-#             headers = {
-#                 'Square-Version': '2023-04-19',
-#                 'Authorization': f'Bearer {token}',
-#                 'Content-Type': 'application/json'
-#             }
-#             data = {
-#                 "given_name": f"{name}",
-#                 "phone_number": f"{phone}"
-#             }
-#             response = requests.post(url, headers=headers, json=data)
-#             response_json = response.json()
-#             if 'customer' in response_json:
-#                 customer_id = response_json['customer']['id']
-#                 return customer_id
-#             else:
-#                 print("Customer creation failed.")
-#         except NameError:
-#             print("Variable 'name or phone' not defined.")
-#         except KeyError:
-#             print("Key 'customer' not found in response JSON.")
+class SchedulerOutput(BaseModel):
+    response: str
 
-#     def get_service_data(self, token, service, location_id):
-#             url = "https://connect.squareup.com/v2/catalog/list"
-#             headers = {
-#                 "Square-Version": "2023-04-19",
-#                 "Authorization": f"Bearer {token}",
-#                 "Content-Type": "application/json"
-#             }
+class Scheduler(BaseAction[SchedulerParameters, SchedulerOutput]):
+    description: str = "Book an appointment for a specific service on a given date and time"
+    action_type: str = "book_appointment"
+    parameters_type: Type[AvailabilityParameters] = AvailabilityParameters
+    response_type: Type[AvailabilityOutput] = AvailabilityOutput
 
-#             response = requests.get(url, headers=headers)
+    def create_customer(self, name, phone, token):
+        try:
+            url = 'https://connect.squareup.com/v2/customers'
+            headers = {
+                'Square-Version': '2023-04-19',
+                'Authorization': f'Bearer {token}',
+                'Content-Type': 'application/json'
+            }
+            data = {
+                "given_name": f"{name}",
+                "phone_number": f"{phone}"
+            }
+            response = requests.post(url, headers=headers, json=data)
+            response_json = response.json()
+            if 'customer' in response_json:
+                customer_id = response_json['customer']['id']
+                return customer_id
+            else:
+                print("Customer creation failed.")
+        except NameError:
+            print("Variable 'name or phone' not defined.")
+        except KeyError:
+            print("Key 'customer' not found in response JSON.")
 
-#             if response.status_code == 200:
-#                 data = response.json()
-#                 item_names = []
-#                 for item in data["objects"]:
-#                     if (item["type"] == "ITEM" and
-#                         item['item_data']['name'] == service and
-#                         item["item_data"]["product_type"] == "APPOINTMENTS_SERVICE" and
-#                         (item.get("present_at_all_locations") or location_id in item.get("present_at_location_ids", []))):
-#                         for variation in item['item_data']['variations']:
-#                                     variation_id = variation['id']
-#                                     service_version = variation['version']
-#                 return variation_id, service_version
-#             else:
-#                 print("Request failed with status code:", response.status_code, response.json())
+    def get_service_data(self, token, service, location_id):
+            url = "https://connect.squareup.com/v2/catalog/list"
+            headers = {
+                "Square-Version": "2023-04-19",
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json"
+            }
+
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                data = response.json()
+                item_names = []
+                for item in data["objects"]:
+                    if (item["type"] == "ITEM" and
+                        item['item_data']['name'] == service and
+                        item["item_data"]["product_type"] == "APPOINTMENTS_SERVICE" and
+                        (item.get("present_at_all_locations") or location_id in item.get("present_at_location_ids", []))):
+                        for variation in item['item_data']['variations']:
+                                    variation_id = variation['id']
+                                    service_version = variation['version']
+                return variation_id, service_version
+            else:
+                print("Request failed with status code:", response.status_code, response.json())
         
-#     def get_team_member_ids(self, token):
-#         url = "https://connect.squareup.com/v2/team-members/search"
-#         headers = {
-#             "Square-Version": "2023-04-19",
-#             "Authorization": f"Bearer {token}",
-#             "Content-Type": "application/json"
-#         }
+    def get_team_member_ids(self, token):
+        url = "https://connect.squareup.com/v2/team-members/search"
+        headers = {
+            "Square-Version": "2023-04-19",
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
 
-#         response = requests.post(url, headers=headers)
-#         response_data = response.json()
+        response = requests.post(url, headers=headers)
+        response_data = response.json()
 
-#         team_members = response_data.get("team_members", [])
-#         team_member_ids = [member.get("id") for member in team_members]
+        team_members = response_data.get("team_members", [])
+        team_member_ids = [member.get("id") for member in team_members]
 
-#         return team_member_ids
+        return team_member_ids
         
-#     def create_booking(self, location, variation_id, service_version, customer_id, booking, token, member_ids):
-#         if location is not None:
-#             headers = {
-#                 'Square-Version': '2023-04-19',
-#                 'Authorization': f'Bearer {token}',
-#                 'Content-Type': 'application/json'
-#             }
-#             for member_id in member_ids:
-#                 try:
-#                     data = {
-#                         "booking": {
-#                             "location_id": f"{location}",
-#                             "location_type": "BUSINESS_LOCATION",
-#                             "appointment_segments": [
-#                                 {
-#                                     "service_variation_id": f"{variation_id}",
-#                                     "team_member_id": f"{member_id}",
-#                                     "service_variation_version": f"{service_version}"
-#                                 }
-#                             ],
-#                             "customer_id": f"{customer_id}",
-#                             "start_at": f"{booking}"
-#                         }
-#                     }
-#                     response = requests.post('https://connect.squareup.com/v2/bookings', headers=headers, data=json.dumps(data))
-#                     print(response.status_code)
-#                     if response.status_code == 200 or response.status_code == 201:
-#                         return response.json()
-#                     else:
-#                         print(response.json())
-#                         continue
-#                 except Exception as e:
-#                     print(f"An error occurred: {e}")
-#                     continue
-#             return {"error": "Could not create booking with any member_id"}
+    def create_booking(self, location, variation_id, service_version, customer_id, booking, token, member_ids):
+        if location is not None:
+            headers = {
+                'Square-Version': '2023-04-19',
+                'Authorization': f'Bearer {token}',
+                'Content-Type': 'application/json'
+            }
+            for member_id in member_ids:
+                try:
+                    data = {
+                        "booking": {
+                            "location_id": f"{location}",
+                            "location_type": "BUSINESS_LOCATION",
+                            "appointment_segments": [
+                                {
+                                    "service_variation_id": f"{variation_id}",
+                                    "team_member_id": f"{member_id}",
+                                    "service_variation_version": f"{service_version}"
+                                }
+                            ],
+                            "customer_id": f"{customer_id}",
+                            "start_at": f"{booking}"
+                        }
+                    }
+                    response = requests.post('https://connect.squareup.com/v2/bookings', headers=headers, data=json.dumps(data))
+                    print(response.status_code)
+                    if response.status_code == 200 or response.status_code == 201:
+                        return response.json()
+                    else:
+                        print(response.json())
+                        continue
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    continue
+            return {"error": "Could not create booking with any member_id"}
     
-#     def parse_booking(self, datetime):
-#         date_time_pt_str = None
-#         if datetime:
-#                     date_time_obj = parser.parse(datetime)
-#                     pt_timezone = timezone('US/Pacific')
-#                     date_time_pt = pt_timezone.localize(date_time_obj)
-#                     date_time_pt_str = date_time_pt.isoformat()
-#         else:
-#             pass
-#         return date_time_pt_str
+    def parse_booking(self, datetime):
+        date_time_pt_str = None
+        if datetime:
+                    date_time_obj = parser.parse(datetime)
+                    pt_timezone = timezone('US/Pacific')
+                    date_time_pt = pt_timezone.localize(date_time_obj)
+                    date_time_pt_str = date_time_pt.isoformat()
+        else:
+            pass
+        return date_time_pt_str
+
+    async def run(
+        self, action_input: ActionInput[AvailabilityParameters]
+    ) -> ActionOutput[AvailabilityOutput]:
         
-#     def run(self, params, token):
-#         name, phone, location_id, service, date, time = params.split("|")
-#         customer_id = self.create_customer(name, phone.replace('-', ''), token)
-#         member_ids = self.get_team_member_ids(token)
-#         variation_id, service_version  = self.get_service_data(token, service, location_id)
-#         date_time = self.parse_booking(date + " " + time)
-#         booking = self.create_booking(location_id, variation_id, service_version, customer_id, date_time, token, member_ids)
-#         return SchedulerOutput(response=str(booking))
+        customer_id = self.create_customer(action_input.params.name, action_input.params.phone.replace('-', ''), action_input.params.token)
+        member_ids = self.get_team_member_ids(action_input.params.token)
+        variation_id, service_version  = self.get_service_data(action_input.params.token, action_input.params.service, action_input.params.location_id)
+        date_time = self.parse_booking(action_input.params.date + " " + action_input.params.time)
+        booking = self.create_booking(action_input.params.location_id, variation_id, service_version, customer_id, date_time, action_input.params.token, member_ids)
+        print(booking)
+
+
+        return ActionOutput(
+            action_type=action_input.action_type,
+            response=SchedulerOutput(response=str(booking)),
+        )
     
 
 # class GetBookings(BaseAction[BookingsOutput]):
