a69d2aa58e51bb59f2128dbf19cefb41d6f79344
DanteNoguez <dante@dezvolta.com.mx>
2023-08-02 21:50:28 -0600

    bookings

--- vocode/streaming/action/chrono_actions.py
+++ vocode/streaming/action/chrono_actions.py
@@ -76,7 +76,55 @@ class BookChrono(BaseAction[BookChronoParameters, BookChronoOutput]):
         )
 
 
+class PatientChronoParameters(BaseModel):
+    doctor: int = Field(..., description="ID of the doctor")
+    patient: str = Field(..., description="Patient's first name")
+    phone: int = Field(..., description="Patient's phone number")
+    token: Optional[str] = Field(None, description="token for the API call.")
+    timezone: Optional[str] = Field(None, description="business' timezone.")
+
+class PatientChronoOutput(BaseModel):
+    response: str
+
+class PatientChrono(BaseAction[PatientChronoParameters, PatientChronoOutput]):
+    description: str = "Create a patient for a doctor"
+    action_type: str = "create_patient"
+    parameters_type: Type[PatientChronoParameters] = PatientChronoParameters
+    response_type: Type[PatientChronoOutput] = PatientChronoOutput
+
+    def patients_create(access_token, doctor=None, first_name=None, cell_phone=None):
+        url = "https://app.drchrono.com/api/patients"
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+        }
+        data = {
+            'doctor': doctor,
+            'first_name': first_name,
+            'gender': 'UNK',
+            'cell_phone': cell_phone,
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+
+        if response.status_code == 201:
+            return response.json()
+        else:
+            response.raise_for_status()
+            return response.json()
+
+    async def run(
+        self, action_input: ActionInput[PatientChronoParameters]
+    ) -> ActionOutput[PatientChronoOutput]:
+        
+        response = self.patients_create(action_input.params.token, action_input.params.doctor, action_input.params.patient, action_input.params.phone)
+
+        return ActionOutput(
+            action_type=action_input.action_type,
+            response=PatientChronoOutput(response=str(response)),
+        )
     
+
+
 def list_appointments(access_token):
 
     base_url = "https://app.drchrono.com/api"
@@ -179,52 +227,6 @@ def list_doctors(access_token):
     else:
         print("Failed to retrieve doctors. Status code: ", response.status_code)
         return response.json()
-    
-
-def appointment_templates_list(token, cursor, doctor, office, page_size, profile):
-    BASE_URL = "https://app.drchrono.com/api"
-    HEADERS = {
-    "Authorization": f"Bearer {token}",
-    "Content-Type": "application/json",
-}
-    params = {
-        "cursor": cursor,
-        "doctor": doctor,
-        "office": office,
-        "page_size": page_size,
-        "profile": profile,
-    }
-    params = {k: v for k, v in params.items() if v is not None}
-
-    response = requests.get(f"{BASE_URL}/appointment_templates_list", headers=HEADERS, params=params)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return response.status_code, response.text
-
-
-def appointment_templates_read(token, id, doctor, office, profile):
-    BASE_URL = "https://app.drchrono.com/api"
-    HEADERS = {
-    "Authorization": f"Bearer {token}",
-    "Content-Type": "application/json",
-}
-    params = {
-        "doctor": doctor,
-        "office": office,
-        "profile": profile,
-        "verbose": True,
-        "available": True,
-    }
-    params = {k: v for k, v in params.items() if v is not None}
-
-    response = requests.get(f"{BASE_URL}/appointment_templates_read/{id}", headers=HEADERS, params=params)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return response.status_code, response.text
     
 
 def appointment_profiles_list(access_token, cursor=None, doctor=None, page_size=None):
@@ -333,3 +335,39 @@ def offices_read(access_token, id, doctor=None):
         return response.json()
     else:
         response.raise_for_status()
+
+def book_appointment(access_token, date, doctor=None, patient=None, office=None, exam_room=None, duration=None):
+        url = "https://app.drchrono.com/api/appointments"
+        headers = {
+            'Authorization': 'Bearer {}'.format(access_token),
+            'Content-Type': 'application/json'
+        }
+
+        params = {
+            'doctor': f'{doctor}',
+            'patient': f'{patient}',
+            'office': f'{office}',
+            'scheduled_time': f'{date}',
+            'exam_room': f'{exam_room}',
+            'duration': f'{duration}',
+        }
+
+        response = requests.post(url, headers=headers, json=params)
+
+        if response.status_code == 200 or 201:
+            print("Appointment successfully created.")
+            return response.json()
+        else:
+            print("Failed to create appointment. Status code: ", response.status_code)
+            return response.json()
+        
+def parse_booking(datetime, tz_str):
+        date_time_pt_str = None
+        if datetime:
+                    date_time_obj = parser.parse(datetime)
+                    pt_timezone = timezone(tz_str)
+                    date_time_pt = pt_timezone.localize(date_time_obj)
+                    date_time_pt_str = date_time_pt.isoformat()
+        else:
+            pass
+        return date_time_pt_str
