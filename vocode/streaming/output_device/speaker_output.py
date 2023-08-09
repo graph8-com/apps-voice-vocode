9d8105fb571d5dea6bc4b651a3842c890e0d10bc
DanteNoguez <dante@dezvolta.com.mx>
2023-08-09 15:46:58 -0600

    no sd

--- vocode/streaming/output_device/speaker_output.py
+++ vocode/streaming/output_device/speaker_output.py
@@ -1,6 +1,5 @@
 import queue
 from typing import Optional
-import sounddevice as sd
 import numpy as np
 
 from .base_output_device import BaseOutputDevice
@@ -21,6 +20,7 @@ class SpeakerOutput(BaseOutputDevice):
         sampling_rate = sampling_rate or int(
             self.device_info.get("default_samplerate", self.DEFAULT_SAMPLING_RATE)
         )
+        import sounddevice as sd
         super().__init__(sampling_rate, audio_encoding)
         self.blocksize = blocksize or self.sampling_rate
         self.stream = sd.OutputStream(
@@ -57,4 +57,4 @@ class SpeakerOutput(BaseOutputDevice):
         cls,
         **kwargs,
     ):
-        return cls(sd.query_devices(kind="output"), **kwargs)
+        return
