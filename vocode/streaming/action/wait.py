4f1e8ffedfcda09d20f9e4e95993833d372dfcb7
Dante Noguez <dantenoguez@gmail.com>
2024-07-28 17:09:58 -0600

    black formatting

--- vocode/streaming/action/wait.py
+++ vocode/streaming/action/wait.py
@@ -31,14 +31,14 @@ class Wait(
     ]
 ):
     description: str = (
-    "Instantly trigger this function if a user indicates a need to pause. "
-    "This includes ALL expressions about waiting, pausing, or getting someone else involved, such as: "
-    "1. Pause requests: 'be right back', 'just a second', 'could you give me a moment?', 'let me move to a quieter place'\n"
-    "2. Involving others: 'let me get him on the phone', 'let me ask my manager'\n"
-    "CRITICAL: NEVER generate ANY verbal message before calling this function. ALWAYS call this function silently, adding a polite "
-    "acknowledgment in the `user_message` parameter of this function. "
-    "This function handles ALL such requests made by humans. Never answer such requests conversationally. "
-    "Do not use this for automated messages or answering machines. "
+        "Instantly trigger this function if a user indicates a need to pause. "
+        "This includes ALL expressions about waiting, pausing, or getting someone else involved, such as: "
+        "1. Pause requests: 'be right back', 'just a second', 'could you give me a moment?', 'let me move to a quieter place'\n"
+        "2. Involving others: 'let me get him on the phone', 'let me ask my manager'\n"
+        "CRITICAL: NEVER generate ANY verbal message before calling this function. ALWAYS call this function silently, adding a polite "
+        "acknowledgment in the `user_message` parameter of this function. "
+        "This function handles ALL such requests made by humans. Never answer such requests conversationally. "
+        "Do not use this for automated messages or answering machines. "
     )
     parameters_type: Type[WaitParameters] = WaitParameters
     response_type: Type[WaitOutput] = WaitOutput
